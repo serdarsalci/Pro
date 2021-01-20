b7fd416b8602510c2368a8cb68165ba7d41bc170
Serdar Salci <serdarsalci@gmail.com>
2021-01-19 20:53:56 -0500

    end of part 12 -- Fetching products from React

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
-import products from '../products'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 const HomeScreen = () => {
+	const [products, setProducts] = useState([])
+
+	// callback function can't be async, thats why create a async function in cb function
+	useEffect(() => {
+		const fetchProducts = async () => {
+			// for this to work dont forget to end proxy in frontend package.json
+			const { data } = await axios.get('/api/products/')
+			setProducts(data)
+		}
+		fetchProducts()
+	}, [])
+
 	return (
 		<>
 			<h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
-const ProductScreen = ({ match }) => {
-	const product = products.find(p => p._id === match.params.id)
+const ProductScreen = props => {
+	const [product, setProduct] = useState({})
+
+	const fetchProduct = async () => {
+		const { data } = await axios.get(`/api/products/${props.match.params.id}`)
+		setProduct(data)
+	}
+
+	useEffect(() => {
+		fetchProduct()
+	}, [])
+
 	return (
 		<>
 			<Link className='btn btn-light my-3' to='/'>
