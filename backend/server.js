e2946eed9fa25e9782b0766ca4aa025ce8b95a4a
Serdar Salci <serdarsalci@gmail.com>
2021-01-20 21:21:24 -0500

    end of part 24 -- Custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
+// import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,20 +17,18 @@ app.get('/', (req, res) => {
 	res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-	res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find(p => p._id == req.params.id)
-	res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.blue.underline
+		`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.blue
+			.underline
 	)
 )
