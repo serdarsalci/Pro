7a353c37e77efe15257fe6b4387ec6840e75b888
Serdar Salci <serdarsalci@gmail.com>
2021-01-27 00:04:11 -0500

    end of part 48 -- Profile Screen Get user details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,11 @@ import {
 	productDetailsReducer,
 } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import {
+	userLoginReducer,
+	userRegisterReducer,
+	userDetailsReducer,
+} from './reducers/userReducers'
 
 const reducer = combineReducers({
 	productList: productListReducer,
@@ -14,7 +18,10 @@ const reducer = combineReducers({
 	cart: cartReducer,
 	userLogin: userLoginReducer,
 	userRegister: userRegisterReducer,
+	userDetails: userDetailsReducer,
 })
+
+const composeEnhancers = composeWithDevTools({ trace: true, traceLimit: 25 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
@@ -42,7 +49,7 @@ const middleware = [thunk]
 const store = createStore(
 	reducer,
 	initialState,
-	composeWithDevTools(applyMiddleware(...middleware))
+	composeEnhancers(applyMiddleware(...middleware))
 )
 
 export default store
