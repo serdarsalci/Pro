b31156a040308cf0e998df4bd1179660c682e251
Serdar Salci <serdarsalci@gmail.com>
2021-02-13 22:48:52 -0500

    end of part 83 --Morgan & Create Review Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import path from 'path'
+import morgan from 'morgan'
 // import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
@@ -16,6 +17,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
